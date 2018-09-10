04b480c72bcd6b06b79612b11c744e3bd9e712d7
Ed Pozharski <pozharski@gmail.com>
2018-09-10 16:39:00 -0400

    Bugfix

--- pdbminer/pisa.py
+++ pdbminer/pisa.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pdbminer
 
-dbsres.ptrn = re.compile("\s*\d*\s*(\S{4})\s*(\d*)\s*(\d*)(.{19})\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)")
+dbsres_ptrn = re.compile("\s*\d*\s*(\S{4})\s*(\d*)\s*(\d*)(.{19})\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)")
 
 class pisa_dbsres_reader(pdbminer.general_reader):
     ''' Reads the single value line produced by PBDe PISA search '''
@@ -14,7 +14,7 @@ class pisa_dbsres_reader(pdbminer.general_reader):
         if line is None:
             chunks = [None, None, '', None, None, None]
         else:
-            chunks = dbsres.ptrn.match(line).groups()[1:]
+            chunks = dbsres_ptrn.match(line).groups()[1:]
             chunks = [int(x) for x in chunks[:2]]+[chunks[2].strip()]+[float(x) for x in chunks[4:7]]
         self.mmsize, self.symnum, self.spacegroup, self.asa, self.bsa, self.deltaG = chunks
     def report(self):
@@ -25,7 +25,7 @@ class pisa_dbsres_pdbase(pdbminer.pdbase):
     def process_codes(self, fpath):
         with open(fpath) as fin:
             for line in fin.readlines():
-                m = dbsres.ptrn.match(line)
+                m = dbsres_ptrn.match(line)
                 if m:
                     code = m.groups()[0]
                     self.insert_new_code(code)
