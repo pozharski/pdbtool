40748eae8131755d8323fde00657aae3a003a9d3
Ed Pozharski <pozharski@gmail.com>
2019-10-30 13:32:53 -0400

    Added bond type selection to hbpisa

--- scripts/hbpisa.py
+++ scripts/hbpisa.py
@@ -14,14 +14,23 @@ parser.add_argument('--sqlpath', default='hbpisa.sqlite',
                     help='HB database file')
 parser.add_argument('--sqlpisa', default='pisa.sqlite',
                     help='PISA database file')
+parser.add_argument('--hbtype', '-b',
+                    help="Hydrogen bond type (mandatory).")
 parser.add_argument('--do-checks', action='store_true',
                     help='Check the PISA database integrity.')
 args = parser.parse_args()
 
 from pdbminer import pisa
 from pdbtool import ReadPDBfile
-from aconts import AnionicToAnionic, hbond_pdbase, HBreader
+from aconts import hbond_pdbase, HBreader, DAKERNEL_PARAMS
 
+if args.hbtype is None:
+    sys.exit('Hydrogen bond type unknown.  Are you sure you understand how this works?')
+try:
+    exec('from aconts import ' + args.hbtype + ' as HydroBonds')
+except ImportError:
+    sys.exit('Hydrogen bond type '+args.hbtype+' is not defined.  Please choose from the following list:\n'+'\n'.join(DAKERNEL_PARAMS.keys()))
+    
 pisabase = pisa.pisa_dbsres_pdbase(args.sqlpisa)
 if args.do_checks:
     pisabase.download_check()
@@ -35,7 +44,7 @@ for code in hpdbase.filter_codes(pisabase.fetch_codes()):
             print(code + ' is an empty structure.  Failed download?',file=sys.stderr)
         else:
             print(code + ' appears to be multi-model, skip', file=sys.stderr)
-    hb = AnionicToAnionic(mol)
+    hb = HydroBonds(mol)
     hpdbase.insert_new_code(code)
     for item in hb.get_readers():
         hpdbase.insert_hb(code, item)
